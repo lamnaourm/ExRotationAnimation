477c1d90aa266873081b5d924deeca69cab2083d
bob <mohammed.lamnaour@gmail.com>
2022-04-26 15:43:00 +0000

    Solution Exercice 1

--- app/src/main/java/com/example/exrotationanimation/MainActivity.java
+++ app/src/main/java/com/example/exrotationanimation/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.RotateAnimation;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView image;
+    EditText e1, e2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         image = findViewById(R.id.img1);
+        e1 = findViewById(R.id.depart);
+        e2 = findViewById(R.id.arrivee);
     }
 
     public void rotate(View view) {
-        RotateAnimation anim = new RotateAnimation(0,180,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+
+        int angleDepart = Integer.valueOf(e1.getText().toString());
+        int angleArrivee = Integer.valueOf(e2.getText().toString());
+
+        RotateAnimation anim = new RotateAnimation(angleDepart,angleArrivee,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
         anim.setDuration(3000);
         anim.setFillAfter(true);
         image.startAnimation(anim);
